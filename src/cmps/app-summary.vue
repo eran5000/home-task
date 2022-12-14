469c3f58bf7d5c11f374918eccc817b1f2f33783
Eran Peled <peled2k@gmail.com>
2022-12-14 16:46:14 +0200

    ver 2

--- src/cmps/app-summary.vue
+++ src/cmps/app-summary.vue
@@ -2,7 +2,7 @@
     <section class="app-summary">
         <img src="../assets/images/illustration-intro.png" alt="">
         <div class="text">
-            <h4>All your files in one secure location, accessible anywhere.</h4>
+            <h2>All your files in one secure location, accessible anywhere.</h2>
             <p>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
         </div>
         <button>Get Started</button>
